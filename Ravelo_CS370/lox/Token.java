22b21e639d7bf6ce691c486bc2be3b137243b07f
Gerswin <162945074+WarzZski@users.noreply.github.com>
2026-01-20 13:44:09 +0000

    Added lox,Scanner,Token,TokenType

--- Ravelo_CS370/lox/Token.java
+++ Ravelo_CS370/lox/Token.java
@@ -1,7 +1,7 @@
 package com.craftinginterpreters.lox;
 
-class Token {
-    final TokenType type; // const
+public class Token {
+    final TokenType type;
     final String lexeme;
     final Object literal;
     final int line;
